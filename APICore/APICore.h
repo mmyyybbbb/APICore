0414faae6095fcfd664dcac5238600fd83164469
Alexej Nenastev <alexey.ne.bcs@mail.ru>
2019-01-11 15:24:29 +0300

    Initial Commit

---
+++ APICore/APICore.h
@@ -0,0 +1,19 @@
+//
+//  APICore.h
+//  APICore
+//
+//  Created by alexej_ne on 11/01/2019.
+//  Copyright © 2019 BCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APICore.
+FOUNDATION_EXPORT double APICoreVersionNumber;
+
+//! Project version string for APICore.
+FOUNDATION_EXPORT const unsigned char APICoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APICore/PublicHeader.h>
+
+
