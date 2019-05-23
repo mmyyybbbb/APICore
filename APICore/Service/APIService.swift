401001016ae6f6086c0279ff4a11bfa7216030f2
vaspondm <ponomarev.bcs@gmail.com>
2019-05-23 11:05:24 +0300

    url params

--- APICore/Service/APIService.swift
+++ APICore/Service/APIService.swift
@@ -112,7 +112,7 @@ fileprivate extension APIService {
         }
         
         if methodParams.bodyParams.isEmpty {
-            return .requestParameters(parameters: methodParams.urlParams, encoding: getBodyEncoding(method))
+            return .requestParameters(parameters: methodParams.urlParams, encoding: URLEncoding.queryString)
         }
         
         return .requestCompositeParameters(bodyParameters: methodParams.bodyParams,
