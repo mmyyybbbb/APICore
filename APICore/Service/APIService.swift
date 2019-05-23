cc13025ea5913dcafec60c2c07a35ca57c90dce8
vaspondm <ponomarev.bcs@gmail.com>
2019-05-23 12:58:11 +0300

    url encoding

--- APICore/Service/APIService.swift
+++ APICore/Service/APIService.swift
@@ -112,7 +112,7 @@ fileprivate extension APIService {
         }
         
         if methodParams.bodyParams.isEmpty {
-            return .requestParameters(parameters: methodParams.urlParams, encoding: URLEncoding.queryString)
+            return .requestParameters(parameters: methodParams.urlParams, encoding: URLEncoding.default)
         }
         
         return .requestCompositeParameters(bodyParameters: methodParams.bodyParams,
