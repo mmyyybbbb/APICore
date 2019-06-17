d48650ab58ae0dac60ff5b96399833f7ec68f98b
Alexej Nenastev <alexey.ne.bcs@mail.ru>
2019-06-17 19:12:21 +0300

    MB-2025

--- APICore/RequestBuilder/RequestBuilder.swift
+++ APICore/RequestBuilder/RequestBuilder.swift
@@ -10,6 +10,11 @@ import RxSwift
 
 import Foundation
 
+public enum RequestBuilderErrorBehavior {
+    case autoRepeatWhen(nsUrlErrorDomainCodeIn: [Int], maxRepeatCount: Int, repeatAfter: TimeInterval)
+    case autoRepeat(maxRepeatCount: Int, repeatAfter: TimeInterval)
+}
+
 open class RequestBuilder<S: APIServiceType>  {
   
     private let method: S.Method
@@ -19,22 +24,86 @@ open class RequestBuilder<S: APIServiceType>  {
     }
   
     public func request() -> Single<Response> {
-        return S.shared.provider.rx.request(method, callbackQueue: DispatchQueue.global())
+        
+        let notifyAboutError: (Error) -> Void = { APICoreManager.shared.requestHttpErrorsPublisher.onNext(extractNSError(from: $0)) }
+        
+        return S.shared.provider.rx
+            .request(method, callbackQueue: DispatchQueue.global())
+            .do(onError: notifyAboutError)
     }
     
+    public func request(onErrorBehavior behavior: RequestBuilderErrorBehavior) -> Single<Response> {
+        
+        func catchError(error: Error) throws -> Single<Response> {
+            return try onCatchError(behavior: behavior, error: error)
+        }
+        
+        return request().catchError(catchError)
+    }
+    
+    /// depricated
     public func requestWithMap<T:Decodable>() -> Single<T> {
         return request()
             .map { response in return try JSONDecoder().decode(T.self, from: response.data) }
             .observeOn(MainScheduler.instance)
     }
-
+    
+    /// depricated
     public func requestWithVoid() -> Single<Void> {
         return request()
             .map { response in return Void() }
             .observeOn(MainScheduler.instance)
     }
-
+    
+    private func onCatchError(behavior: RequestBuilderErrorBehavior, error: Error) throws  -> Single<Response> {
+        let nsError = extractNSError(from: error)
+        
+        guard nsError.domain == NSURLErrorDomain else { throw error }
+        
+        switch behavior {
+        case let .autoRepeatWhen(nsUrlErrorDomainCodeIn, maxRepeatCount, repeatAfter):
+            guard nsUrlErrorDomainCodeIn.contains(nsError.code), maxRepeatCount > 0  else { break }
+            let behavior = RequestBuilderErrorBehavior.autoRepeatWhen(nsUrlErrorDomainCodeIn: nsUrlErrorDomainCodeIn,
+                                                                      maxRepeatCount: maxRepeatCount-1,
+                                                                      repeatAfter: repeatAfter)
+            return request(onErrorBehavior:  behavior)
+                .delaySubscription(repeatAfter, scheduler: ConcurrentDispatchQueueScheduler(qos: .userInitiated)) 
+            
+        case let .autoRepeat(maxRepeatCount, repeatAfter):
+            guard maxRepeatCount > 0  else { break }
+            let behavior = RequestBuilderErrorBehavior.autoRepeat(maxRepeatCount: maxRepeatCount-1, repeatAfter: repeatAfter)
+            return request(onErrorBehavior:  behavior)
+                .delaySubscription(repeatAfter, scheduler: ConcurrentDispatchQueueScheduler(qos: .userInitiated))
+        }
+        
+        throw error
+    }
 }
 
+fileprivate func extractNSError(from error: Error) -> NSError {
+    if case MoyaError.underlying(let err, _) = error  {
+        return err as NSError
+    }
+    return error as NSError
+}
+
+public extension Single where Element == Response {
+    
+    func mapTo<T:Decodable>(on scheduler: ImmediateSchedulerType = MainScheduler.instance) -> Single<T> {
+        return self
+            .asObservable()
+            .map { response in return try JSONDecoder().decode(T.self, from: response.data) }
+            .asSingle()
+            .observeOn(scheduler)
+    }
+    
+    func mapToVoid(on scheduler: ImmediateSchedulerType = MainScheduler.instance) -> Single<Void> {
+        return self
+            .asObservable()
+            .map { response in return Void() }
+            .asSingle()
+            .observeOn(scheduler)
+    }
+}
 
 
--- APICoreTests/APICoreTests.swift
+++ APICoreTests/APICoreTests.swift
@@ -20,7 +20,9 @@ class APICoreTests: XCTestCase, AuthTokenProvider {
         APICoreObjectContainer.instanceLazyInit.register(config)
     }
     
-     var token: AuthToken? = "token"
+    let bag = DisposeBag()
+    
+    var token: AuthToken? = "token"
     
     override func tearDown() {
         APICoreObjectContainer.releaseInstance()
@@ -28,7 +30,7 @@ class APICoreTests: XCTestCase, AuthTokenProvider {
  
     func test_customConfigurationForService() {
         // запрос должен отвалиться потомучту мы переопределим конфигурацию для сервиса и укажем в ней некорректный url
-        let configurator = ReqresServicesConfigurator(baseUrl: "https://reqres2222222.in") // плохой url
+        let configurator = ReqresServicesConfigurator(baseUrl: "https://my.broker2.ru") // плохой url
         APICoreObjectContainer.instanceLazyInit.register(configurator: configurator, for: ReqresUserAPI.self)
         
         let request = RequestBuilder(ReqresUserAPI.self, .single(id: 2)).request()
@@ -42,7 +44,36 @@ class APICoreTests: XCTestCase, AuthTokenProvider {
                 XCTFail("No expected result")
                 return
             }
-
+            
+            XCTAssertEqual((err as NSError).domain, "NSURLErrorDomain", "")
+        }
+    }
+    
+    func test_noInternetConnection() {
+        let configurator = ReqresServicesConfigurator(baseUrl: "https://google.com")
+        APICoreObjectContainer.instanceLazyInit.register(configurator: configurator, for: ReqresUserAPI.self)
+        
+        let behavior: RequestBuilderErrorBehavior = .autoRepeatWhen(nsUrlErrorDomainCodeIn: [NSURLErrorNotConnectedToInternet],
+                                                                    maxRepeatCount: 3,
+                                                                    repeatAfter: 5)
+        
+        APICoreManager.shared.requestHttpErrors
+            .subscribe(onNext: { error in print("[GET] \(error)") })
+            .disposed(by: bag)
+        
+        let request = RequestBuilder(ReqresUserAPI.self, .single(id: 2)).request(onErrorBehavior: behavior)
+        let result = request.toBlocking().materialize()
+        
+        
+        switch result {
+        case .completed:
+            XCTFail("No expected result")
+        case let .failed(_, error):
+            
+            guard case MoyaError.underlying(let err, _) = error else  {
+                XCTFail("No expected result")
+                return
+            }
             XCTAssertEqual((err as NSError).domain, "NSURLErrorDomain", "")
         }
     }
@@ -50,7 +81,7 @@ class APICoreTests: XCTestCase, AuthTokenProvider {
     func test_simpleRequest() {
         
         do {
-            let request:Single<Response<User>> = RequestBuilder(ReqresUserAPI.self, .single(id: 2)).map()
+            let request:Single<Response<User>> = RequestBuilder(ReqresUserAPI.self, .single(id: 2)).requestWithMap()
             let result = try request.toBlocking().first()
             XCTAssertNotNil(result, "Data model is nil")
         } catch {
@@ -73,7 +104,7 @@ class APICoreTests: XCTestCase, AuthTokenProvider {
             ReqresUserAPI.shared.useMocksIfSetted = true
             ReqresUserAPI.shared.setMock(for: .single, value: response)
             
-            let request:Single<Response<User>> = RequestBuilder(ReqresUserAPI.self, .single(id: 2)).map()
+            let request:Single<Response<User>> = RequestBuilder(ReqresUserAPI.self, .single(id: 2)).requestWithMap()
             let result = try request.toBlocking().first()
             XCTAssertEqual(result?.data.id, 222, "NO mocks")
         } catch {
