9d4ebc77470494f7a2b3e47cf3d34a05e037e1c6
Rishat Yakushev <rishat.yakushev@gmail.com>
2020-02-12 13:52:37 +0300

    MB-4646 Правки

--- APICore/Service/APIService.swift
+++ APICore/Service/APIService.swift
@@ -116,7 +116,7 @@ fileprivate extension APIService {
         if let multipartData = method.multipart {
             let formData = multipartData.map { MultipartFormData(provider: .data($0.data),
                                                                  name: $0.name,
-                                                                 fileName: "file.jpg",
+                                                                 fileName: $0.fileName,
                                                                  mimeType: $0.mimeType) }
                 
             return .uploadCompositeMultipart(formData, urlParameters: methodParams.urlParams)
