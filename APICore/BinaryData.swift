eaa5050ff12c41070faa3c293a96ad6a8681c5c1
Rishat Yakushev <rishat.yakushev@gmail.com>
2020-02-13 16:16:35 +0300

    MB-4741 Удален fileName

--- APICore/BinaryData.swift
+++ APICore/BinaryData.swift
@@ -18,14 +18,6 @@ public enum BinaryData {
         }
     }
     
-    public var fileName: String? {
-        switch self {
-        case .jpeg: return "file.jpg"
-        case .customName: return "file.jpg"
-        default: return nil
-        }
-    }
-    
     public var mimeType: String? {
         switch self {
         case .jpeg: return "image/jpeg"
