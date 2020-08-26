f0c155e16bc338528e3b69521b20d7c6fdc3be42
Yaroslav Yashin <yaroslav.yashin@me.com>
2020-08-26 17:12:57 +0300

    MB-8810 Фикс бага form-data.

--- APICore/BinaryData.swift
+++ APICore/BinaryData.swift
@@ -8,29 +8,33 @@
 
 public enum BinaryData {
     case jpeg(Data)
-    case custom(Data, mimeType: String)
-    case customName(Data, mimeType: String?, key: String)
+    case custom(Data, mimeType: String?, key: String, fileName: String?)
     
     public var name: String {
         switch self {
-        case .customName(_, _, let key): return key
-        default: return "file"
+        case .jpeg(_): return "file"
+        case .custom(_, _, let key, _): return key
         }
     }
     
     public var mimeType: String? {
         switch self {
         case .jpeg: return "image/jpeg"
-        case .custom(_, let str): return str
-        case .customName(_, let mimeType, _): return mimeType
+        case .custom(_, let mimeType, _, _): return mimeType
         }
     }
     
     public var data: Data {
         switch self {
         case .jpeg(let data): return data
-        case .custom(let data, _): return data
-        case .customName(let data, _, _): return data
+        case .custom(let data, _, _, _): return data
+        }
+    }
+    
+    public var fileName: String? {
+        switch self {
+        case .jpeg(_): return "file.jpg"
+        case .custom(_, _, _, let filename): return filename
         }
     }
 }
